bbd2ce1f31ccf11219dea1ca06341faf1c74a239
LuisLizardo <LuisLizardo73@gmail.com>
2023-10-03 12:36:09 -0400

    Getting Started
    Ye

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function SideBar() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
+  const names = ["Lelo", "Jackson", "Luis", "Jessie", "Joey"]
+  return (
+    <>
+      <h1 className="title">{name}</h1>
+      <button onClick={ () => setCount(count+1) }> click</button>
+      <button onClick={ () => setName("Luis") }> clickForName</button>
+      <p>{count }</p>
+      <ul>
+        {names.map(name => (
+          <li>
+            {name}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default SideBar
