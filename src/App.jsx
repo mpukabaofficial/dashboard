96a263eb92c66711a2d3b642bec5f6aaf3d82a23
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-10-06 15:46:40 -0400

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SideBar from './components/SideBar'
-import './SideBar.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import SideBar from "./components/SideBar";
+import "./SideBar.css";
 import "./App.css";
 import Carrousel from "./components/Carrousel";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("")
-  const names = ["Lelo", "Jackson", "Luis", "Jessie", "Joey"]
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const names = ["Lelo", "Jackson", "Luis", "Jessie", "Joey"];
   function show() {
-  document.getElementById('sidebar').classList.toggle('active');
-}
+    document.getElementById("sidebar").classList.toggle("active");
+  }
   return (
     <>
-    <SideBar></SideBar>
-     <Carrousel />
+      <SideBar></SideBar>
+      <Carrousel />
     </>
+  );
+}
 
 export default App;
-
