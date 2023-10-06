87382d83111588e5a241068f5abb15d3f721ba40
LuisLizardo <LuisLizardo73@gmail.com>
2023-10-06 15:29:41 -0400

    added a sidebar with animation, and template buttons using images with links

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SideBar from './SideBar'
+import SideBar from './components/SideBar'
 import './SideBar.css'
-
 
 function App() {
   const [count, setCount] = useState(0)
