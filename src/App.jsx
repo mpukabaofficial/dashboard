73ac00a54115149fa067c3a4a8848fa6e36e43ca
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-10-09 15:20:29 -0400

    Updated vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import SideBar from './components/SideBar'
-import './SideBar.css'
-import TopBar from './TopBar'
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import SideBar from "./components/SideBar";
-import "./SideBar.css";
-
-import "./App.css";
+import TopBar from "./TopBar";
 import Carrousel from "./components/Carrousel";
 
-
 function App() {
-
-
   return (
     <>
       <TopBar></TopBar>
-          <SideBar></SideBar>
+      <SideBar></SideBar>
       <Carrousel />
     </>
   );
