58b671e1627e61c1446b0b63525f3f5b4c50598b
JessRamz25 <jessramiisd.9th@gmail.com>
2023-10-11 15:29:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SideBar from "./components/SideBar";
 import TopBar from "./TopBar";
 
 import Carrousel from "./components/Carrousel";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <SideBar></SideBar>
       <Carrousel />
+      <LoginPage/>
 
     </>
   );
