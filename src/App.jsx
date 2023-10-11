84e7c19eb39b22718ccb2d7f82b58e4ed1004909
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-10-11 15:24:13 -0400

    did some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import SideBar from "./components/SideBar";
 import TopBar from "./TopBar";
 
 import Carrousel from "./components/Carrousel";
+import Layout from "./Layout";
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
 
       <TopBar></TopBar>
 
       <SideBar></SideBar>
       <Carrousel />
 
+=======
+      <Layout></Layout>
+>>>>>>> Stashed changes
     </>
   );
 }
