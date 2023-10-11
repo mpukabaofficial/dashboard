f83b4dcc32483936c58f7caca803dd440c4d9855
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-10-11 15:36:30 -0400

    committing sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-
 import "./App.css";
-import SideBar from "./components/SideBar";
-import TopBar from "./TopBar";
-
 import Carrousel from "./components/Carrousel";
 
 function App() {
   return (
     <>
-
-      <TopBar></TopBar>
-
-      <SideBar></SideBar>
-      <Carrousel />
-
+      <Carrousel></Carrousel>
     </>
   );
 }
