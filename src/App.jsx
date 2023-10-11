25aae038796d9ca5fe306de9f24d864ccf5e93be
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-10-11 15:56:24 -0400

    no changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import Carrousel from "./components/Carrousel";
 
-import Layout from "./Layout";
-
 function App() {
   return (
     <>
-
-      <Layout></Layout>
+      <Carrousel></Carrousel>
     </>
   );
 }
