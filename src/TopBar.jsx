5096160a6f691506fb0c0dc4db6ec3877c2bf5ea
EnderOwlPlays <40880924+JacksonKiino-Terburg@users.noreply.github.com>
2023-10-16 16:01:07 -0400

    final

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './TopBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser,faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
 
 function TopBar() {
   return (
     <>
       <div class="topnav">
-      <a href=''><img src="https://hornethq.kzoo.edu/Student/K-Images/HornetHQ5.svg" alt="Hornet HQ" width="300" height="80"/></a>
-      <div className='TopBar'>
-      <a>Profile</a>
-      <a>Sign Out</a>
-      </div>
+        <a href=''><img src="https://hornethq.kzoo.edu/Student/K-Images/HornetHQ5.svg" alt="Hornet HQ" width="300" height="80"/></a>
+        <div className='TopBar'>
+          
+          <a><FontAwesomeIcon icon={faUser} style={{color: "#ff8000",}} /> Profile</a>
+          <a><FontAwesomeIcon icon={faRightFromBracket} style={{color: "#ff8040",}} /> Sign Out</a>
+        </div>
       </div> 
     </>
   )
