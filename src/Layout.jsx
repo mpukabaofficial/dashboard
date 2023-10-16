de63dc6fe3109fad3a6ce01f812bfd6b519a1ddd
EnderOwlPlays <40880924+JacksonKiino-Terburg@users.noreply.github.com>
2023-10-16 15:06:35 -0400

    slightly better looking top bar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Layout.css";
+import TopBar from "./TopBar";
 
 const Layout = () => {
   return (
     <div className="container">
       <div className="Navbar">NavBAr</div>
       <div className="main">
-        <div className="header">header</div>
+        <div className="header"><TopBar></TopBar></div>
         <div>
           <div className="carousel">carousel</div>
           <div className="schedule">Schedule</div>
--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './TopBar.css'
 
 function TopBar() {
   return (
     <>
       <div class="topnav">
       <a href=''><img src="https://hornethq.kzoo.edu/Student/K-Images/HornetHQ5.svg" alt="Hornet HQ" width="300" height="80"/></a>
-      <button className='signin'>Sign In</button>
+      <div className='TopBar'>
+      <a href="">Sign In</a>
+      </div>
+      
+
       </div> 
     </>
   )
