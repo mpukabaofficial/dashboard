ed24ce714fd28469533eebc0a80d43841cf44cee
LuisLizardo <LuisLizardo73@gmail.com>
2023-10-27 14:52:34 -0400

    Sidebar WIP
    Problems with Icons displaying

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,7 +7,7 @@ import employmentbutton from '../assets/employment.png'
 import  academicsbutton from '../assets/academics.png'
 import useroptionsbutton from '../assets/useroptions.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faDollarSign, faHouse, faNewspaper } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faDollarSign, faGear, faGraduationCap, faHouse, faMoneyBillTrendUp, faNewspaper, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -15,41 +15,57 @@ const Sidebar = () => {
   const toggleSidebar = () => {
     setIsActive(!isActive);
   };
-
+  
   const arraySidebar = [
-    {
-      name: "Billing",
-      icons: faDollarSign,
-      link: "/",
-    },
     {
       name: "Home",
       icons: faHouse,
       link: "/",
     },
     {
-      name: "News",
-      icons: faNewspaper,
+      name: "Academics",
+      icons: faGraduationCap,
       link: "/",
     },
+    {
+      name: "Finnancial Information",
+      icons: faMoneyBillTrendUp,
+      link: "/",
+    },
+    {
+      name: "Employment",
+      icons: faDollarSign,
+      link: "/",
+    },
+    {
+      name: "Settings",
+      icons: faGear,
+      link: "/",
+    },
+    
   ];
   return (
     <div id="sidebar" className={isActive ? 'active' : ''}>
-     <div className="toggle-btn" onClick={toggleSidebar}>
-        <FontAwesomeIcon icon={faBars} />
-        <ul>
-          {arraySidebar.map((item, index) => (
-            <li key={index}>
-              <a href={item.link}>
-              <FontAwesomeIcon icon={item.icons} />
-                {isActive ? item.name : ""}</a></li>
-          ))}
-        </ul>
+      <div className="toggle-btn" onClick={toggleSidebar}>
+        <FontAwesomeIcon icon={faBars} style={{ fontSize: '24px' }} />
       </div>
-      </div>
-    
+      <ul>
+        {arraySidebar.map((item, index) => (
+          <li key={index}>
+            <a
+              href={item.link}
+              style={{
+                marginLeft: isActive ? '0' : '-100px', 
+              }}
+            >
+              <FontAwesomeIcon icon={item.icons} className="fa-icon"style={{ marginRight: '10px' }} />
+              {isActive && item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
-
 
 export default Sidebar;
